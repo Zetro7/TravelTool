f7196491b779f70de1b76961e52485880f2159ac
bit4261 <78128675+bit4261@users.noreply.github.com>
2021-02-17 00:16:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-const data = null;
-
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/ALB-sky/CAK-sky/2021-03-01?inboundpartialdate=2021-03-04", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "eee204e8b3msh0f6a1cc96c72584p13af77jsnad25c961126a",
+		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
 	}
+})
+.then(response => response.json())
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
 });
-
-xhr.open("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/referral/v1.0/%7Bcountry%7D/%7Bcurrency%7D/%7Blocale%7D/%7Boriginplace%7D/%7Bdestinationplace%7D/%7Boutboundpartialdate%7D/%7Binboundpartialdate%7D?shortapikey=ra66933236979928&apiKey=%7Bshortapikey%7D");
-xhr.setRequestHeader("x-rapidapi-key", "eee204e8b3msh0f6a1cc96c72584p13af77jsnad25c961126a");
-xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
-
-xhr.send(data);
