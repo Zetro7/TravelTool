a1cf6e27621627eb5fb28567d5def2b5b2575d83
bit4261 <78128675+bit4261@users.noreply.github.com>
2021-02-17 00:13:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/PIT-sky/MIA-sky/2021-03-01?inboundpartialdate=2021-03-04", {
+fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/ALB-sky/CAK-sky/2021-03-01?inboundpartialdate=2021-03-04", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "eee204e8b3msh0f6a1cc96c72584p13af77jsnad25c961126a",
