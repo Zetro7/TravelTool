4294233ee7be092849ea7543e5c10bc2aebdbe4e
rminerd <78128679+rminerd@users.noreply.github.com>
2021-03-04 21:07:05 -0500

    Update hotels.js

--- hotels.js
+++ hotels.js
@@ -1,4 +1,4 @@
-fetch(urlFinal, {
+fetch("https://hotels4.p.rapidapi.com/properties/get-details?id=424023&locale=en_US&currency=USD&checkOut=2020-01-15&adults1=1&checkIn=2020-01-08", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "fe2be3ebc9msha38fa8cac5dbaddp1dd0dejsn9ace77793bbf",
