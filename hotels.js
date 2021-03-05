409d337dd23cfd58260c4dc94358118d130da36f
rminerd <78128679+rminerd@users.noreply.github.com>
2021-03-04 22:41:28 -0500

    Update hotels.js

--- hotels.js
+++ hotels.js
@@ -1,4 +1,4 @@
-fetch("https://hotels4.p.rapidapi.com/properties/get-details?id=424023&locale=en_US&currency=USD&checkOut=2020-01-15&adults1=1&checkIn=2020-01-08", {
+fetch("https://hotels4.p.rapidapi.com/locations/search?query=pittsburgh&locale=en_US", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "fe2be3ebc9msha38fa8cac5dbaddp1dd0dejsn9ace77793bbf",
