cf056c65559b6ab88a5c86b9801c1ebfb9e8b931
rminerd <78128679+rminerd@users.noreply.github.com>
2021-03-04 23:05:30 -0500

    Update hotels.js

--- hotels.js
+++ hotels.js
@@ -1,4 +1,4 @@
-fetch("https://hotels4.p.rapidapi.com/properties/list?destinationId=1474927&pageNumber=1&checkIn=2021-03-12&checkOut=2020-03-14&pageSize=25&adults1=1&currency=USD&locale=en_US&sortOrder=PRICE", {
+fetch("https://hotels4.p.rapidapi.com/properties/list?destinationId=1474927&pageNumber=1&checkIn=2021-03-12&checkOut=2021-03-14&pageSize=25&adults1=1&currency=USD&locale=en_US&sortOrder=PRICE", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "fe2be3ebc9msha38fa8cac5dbaddp1dd0dejsn9ace77793bbf",
