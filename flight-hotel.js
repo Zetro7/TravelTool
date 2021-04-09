e4933c9ce0a4bc879848888bb6ebcecd92cbdf39
bit4261 <78128675+bit4261@users.noreply.github.com>
2021-04-08 23:24:19 -0400

    Update flight-hotel.js

--- flight-hotel.js
+++ flight-hotel.js
@@ -7,24 +7,26 @@ var originSplit = splitNames[1].split("&");
 var destSplit = splitNames[2].split("&");
 var departSplit = splitNames[3].split("&");
 var returnSplit = splitNames[4].split("&");
+var destHold = destSplit[0].split("+");
 //var cityOnlySplit = splitNames[5].split("-");
 
 //These variables are now user defined and sent to the API
 //https://hotels4.p.rapidapi.com/locations/search?query=new%20york&locale=en_US
 var origin = originSplit[0];
-var dest = destSplit[0];
+var dest = destHold[0].concat("%20".concat(destHold[1]));
 var departDate = departSplit[0];
 var returnDate = returnSplit[0];
+console.log(dest);
 //var cityOnly = cityOnlySplit[0];
 //console.log(cityOnly);
 var hotelLocationURL = "https://hotels4.p.rapidapi.com/locations/search?query=";
 var finalHotelLocationURL = hotelLocationURL.concat(dest).concat("&locale=en_US");
-//console.log(finalHotelLocationURL);
+console.log(finalHotelLocationURL);
 
 fetch(finalHotelLocationURL, {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "eee204e8b3msh0f6a1cc96c72584p13af77jsnad25c961126a",
+		"x-rapidapi-key": "c0fc24bd79msh019f02987794e44p153e68jsncddddeab7434",
 		"x-rapidapi-host": "hotels4.p.rapidapi.com"
 	}
 })
@@ -39,7 +41,7 @@ fetch(finalHotelLocationURL, {
     //console.log(userData);
 
 var hotelDataURL = "https://hotels4.p.rapidapi.com/properties/list?destinationId=";
-var finalHotelDataURL = hotelDataURL.concat(destinationID).concat("&pageNumber=1&").concat("checkIn=").concat(departDate).concat("&").concat("checkOut=").concat(returnDate).concat("&pageSize=25&adults1=1&currency=USD&locale=en_US&sortOrder=PRICE");
+var finalHotelDataURL = hotelDataURL.concat(destinationID).concat("&pageNumber=1&").concat("checkIn=").concat(departDate).concat("&").concat(returnDate).concat("&pageSize=25&adults1=1&currency=USD&locale=en_US&sortOrder=PRICE");
 console.log(finalHotelDataURL);
 /*
 destinationID + pageNumber=1(static) + checkIn + checkOut + pageSize=25(static) + adults1=1(static for now) + currency=USD(static) + locale=en_US(static)+sortOrder=PRICE(static)
@@ -49,7 +51,7 @@ destinationID + pageNumber=1(static) + checkIn + checkOut + pageSize=25(static) 
 fetch(finalHotelDataURL, {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "eee204e8b3msh0f6a1cc96c72584p13af77jsnad25c961126a",
+		"x-rapidapi-key": "c0fc24bd79msh019f02987794e44p153e68jsncddddeab7434",
 		"x-rapidapi-host": "hotels4.p.rapidapi.com"
 	}
 })
@@ -69,20 +71,21 @@ fetch(finalHotelDataURL, {
     var temp2;
     var temp3;
     var temp4;
-    var temp5;
-	
+ 
     var table = document.getElementById("hotelTable");
     
-    for(var n = 0; n < hotelCount/8; n++){
+    for(var n = 0; n < hotelCount/10; n++){
         
     
         var finalGoogleURL = initGoogleURL.concat(response.data.body.searchResults.results[n].name);
         var googleHyperLink = "Book Here";
-        temp5 = googleHyperLink.link(finalGoogleURL);
+       
         temp1 = response.data.body.searchResults.results[n].name;
         temp2 = response.data.body.searchResults.results[n].ratePlan.price.current;
          temp3 = response.data.body.searchResults.results[n].starRating;
-        for(var l = 0; l < hotelCount/8; l++)
+        var radioButton = document.createElement("input");
+        radioButton.setAttribute("type", "radio");
+        for(var l = 0; l < hotelCount/10; l++)
         {
             temp4 = response.data.body.searchResults.results[n].roomsLeft;
             if(temp4 == undefined)
@@ -94,14 +97,15 @@ fetch(finalHotelDataURL, {
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
+        var cell4 = row.insertCell(3); 
         var cell5 = row.insertCell(4);
-    
+        
+        cell5.appendChild(radioButton);
         cell1.innerHTML = temp1;
         cell2.innerHTML = temp2;
         cell3.innerHTML = temp3;
         cell4.innerHTML = temp4;
-        cell5.innerHTML = temp5;
+      
      
         
     }
